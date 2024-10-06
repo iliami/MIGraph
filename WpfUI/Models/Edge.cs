4614a4878776e497bef1b221c1294f1cc1a679fd
iliami <ilia.mi1908@gmail.com>
2024-10-06 22:10:38 +0400

    Добавил модели Вершины и Ребра

---
+++ WpfUI/Models/Edge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUI.Models
+{
+    public class Edge(string name, Vertex firstVertex, Vertex secondVertex)
+    {
+        public string Name { get; set; } = name;
+        public Vertex FirstVertex { get; set; } = firstVertex;
+        public Vertex SecondVertex { get; set; } = secondVertex;
+
+        public static bool operator ==(Edge left, Edge right)
+            => left.Name == right.Name 
+            && left.FirstVertex == right.FirstVertex 
+            && left.SecondVertex == right.SecondVertex;
+        public static bool operator !=(Edge left, Edge right)
+            => !(left == right);
+    }
+}
---
+++ WpfUI/Models/Vertex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUI.Models
+{
+    public class Vertex(string name, double xPos, double yPos)
+    {
+        public string Name { get; set; } = name;
+        public double XPos { get; set; } = xPos;
+        public double YPos { get; set; } = yPos;
+
+
+        public static bool operator ==(Vertex left, Vertex right)
+            => left.Name == right.Name 
+            && left.XPos == right.XPos 
+            && left.YPos == right.YPos;
+        public static bool operator !=(Vertex left, Vertex right)
+            => !(left == right);
+    }
+}
