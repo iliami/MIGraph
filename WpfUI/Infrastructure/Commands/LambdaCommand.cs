ad29adb0c98dfb1c38b9c855a960da4a4d34ee2f
iliami <ilia.mi1908@gmail.com>
2024-10-06 20:46:23 +0400

    Добавил лямбда-команду (LambdaCommand / RelayCommand)

---
+++ WpfUI/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUI.Infrastructure.Commands.Base;
+
+namespace WpfUI.Infrastructure.Commands
+{
+    public class LambdaCommand(Action<object?>? execute, Predicate<object?>? canExecute = null) : Command
+    {
+        private readonly Predicate<object?> canExecute = canExecute ?? (_ => true);
+        private readonly Action<object?> execute = execute ?? throw new ArgumentNullException(nameof(execute));
+
+        public override bool CanExecute(object? parameter)
+            => canExecute.Invoke(parameter);
+
+        public override void Execute(object? parameter)
+            => execute.Invoke(parameter);
+    }
+}
