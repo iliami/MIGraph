1407ae7bd29ec51dee0df4f78d2dfbdb32e9fa75
iliami <ilia.mi1908@gmail.com>
2024-10-06 21:06:04 +0400

    Реализация команды - команда для закрытия приложения.

--- WpfUI/ViewModels/MainWindowViewModel.cs
+++ WpfUI/ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
+using System.Windows.Input;
+using WpfUI.Infrastructure.Commands;
 using WpfUI.ViewModels.Base;
 
 namespace WpfUI.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
+        #region Модели-представления
+
         #region Title
-        
+
         private string _Title = "Заголовок окна";
-        public string Title 
-        { 
-            get => _Title; 
-            set => Set(ref _Title, value); 
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
         }
 
         #endregion
@@ -30,5 +30,33 @@ namespace WpfUI.ViewModels
         }
 
         #endregion
+
+        #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; private set; }
+
+        public bool CanCloseApplicationCommandExecute(object? parameter) => true;
+
+        public void OnCloseApplicationCommandExecuted(object? parameter)
+            => Application.Current.Shutdown();
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(
+                OnCloseApplicationCommandExecuted,
+                CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
