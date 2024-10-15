d2a473e1de2ce0230141a3856e29cd62f62d6962
iliami <ilia.mi1908@gmail.com>
2024-10-15 21:54:11 +0400

    Изменил модели для поддержки отслеживания изменений их состояния

--- WpfUI/Models/Edge.cs
+++ WpfUI/Models/Edge.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfUI.Models.Base;
 
 namespace WpfUI.Models
 {
-    public class Edge(string name, Vertex firstVertex, Vertex secondVertex)
+    public class Edge(string name, Vertex firstVertex, Vertex secondVertex) : INPC
     {
-        public string Name { get; set; } = name;
-        public Vertex FirstVertex { get; set; } = firstVertex;
-        public Vertex SecondVertex { get; set; } = secondVertex;
+        #region Name
 
+        private string _Name = name;
+        public string Name
+        {
+            get => _Name;
+            set => Set(ref _Name, value);
+        }
+
+        #endregion
+
+        #region FirstVertex
+
+        private Vertex _FirstVertex = firstVertex;
+        public Vertex FirstVertex
+        {
+            get => _FirstVertex;
+            set => Set(ref _FirstVertex, value);
+        }
+
+        #endregion
+
+        #region SecondVertex
+
+        private Vertex _SecondVertex = secondVertex;
+        public Vertex SecondVertex
+        {
+            get => _SecondVertex;
+            set => Set(ref _SecondVertex, value);
+        }
+
+        #endregion
+        
         public static bool operator ==(Edge left, Edge right)
             => left.Name == right.Name
             && left.FirstVertex == right.FirstVertex
--- WpfUI/Models/Vertex.cs
+++ WpfUI/Models/Vertex.cs
@@ -3,18 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfUI.Models.Base;
 
 namespace WpfUI.Models
 {
-    public class Vertex(string name, double xPos, double yPos, int diameter = 20)
+    public class Vertex(string name, double xPos, double yPos, int diameter = 20) : INPC
     {
-        public string Name { get; set; } = name;
-        public double XPos { get; set; } = xPos;
-        public double CanvasLeft { get; set; } = xPos - diameter / 2;
-        public double YPos { get; set; } = yPos;
-        public double CanvasTop { get; set; } = yPos - diameter / 2;
-        public int Diameter { get; set; } = diameter;
+        #region Name
 
+        private string _Name = name;
+        public string Name
+        {
+            get => _Name;
+            set => Set(ref _Name, value);
+        }
+
+        #endregion
+
+        #region XPos
+
+        private double _XPos = xPos;
+        public double XPos
+        {
+            get => _XPos;
+            set
+            {
+                CanvasLeft = _XPos - Diameter / 2;
+                Set(ref _XPos, value);
+            }
+        }
+
+        #endregion
+
+        #region CanvasLeft
+
+        private double _CanvasLeft xPos - diameter / 2;
+        public double CanvasLeft
+        {
+            get => _CanvasLeft;
+            set => Set(ref _CanvasLeft, value);
+        }
+
+        #endregion
+
+        #region YPos
+
+        private double _YPos;
+        public double YPos
+        {
+            get => _YPos;
+            set
+            {
+                CanvasTop = _YPos - Diameter / 2;
+                Set(ref _YPos, value);
+            }
+        }
+
+        #endregion
+
+        #region CanvasTop
+
+        private double _CanvasTop = yPos - diameter / 2;
+        public double CanvasTop
+        {
+            get => _CanvasTop;
+            set => Set(ref _CanvasTop, value);
+        }
+
+        #endregion
+
+        #region Diameter
+
+        private int _Diameter = diameter;
+        public int Diameter
+        {
+            get => _Diameter;
+            set => Set(ref _Diameter, value);
+        }
+
+        #endregion
 
         public static bool operator ==(Vertex left, Vertex right)
             => left.Name == right.Name 
