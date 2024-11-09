15c1684536b1e641f3261908d67aa3f1268fbe20
iliami <ilia.mi1908@gmail.com>
2024-11-10 00:39:24 +0400

    Добавил ToString для моделей вершины и ребра

--- WpfUI/Models/Edge.cs
+++ WpfUI/Models/Edge.cs
@@ -6,5 +6,15 @@
         public Vertex FirstVertex { get; set; } = firstVertex;
         public Vertex SecondVertex { get; set; } = secondVertex;
         public double Weight { get; set; } = 0;
+
+        public override string ToString()
+        {
+            var result = ( Weight == 0 ) ? 
+                $"Имя {Name:16} | Вершины [ {FirstVertex.Name:16} ({FirstVertex.X:f2}; {FirstVertex.Y:f2}) | " +
+                $"{SecondVertex.Name:16} ({SecondVertex.X:f2}; {SecondVertex.Y:f2}) ]" : 
+                $"Имя {Name:16} | Вершины [ {FirstVertex.Name:16} ({FirstVertex.X:f2}; {FirstVertex.Y:f2}) | " +
+                $"{SecondVertex.Name:16} ({SecondVertex.X:f2}; {SecondVertex.Y:f2}) ] | Вес {Weight}";
+            return result;
+        }
     }
 }
--- WpfUI/Models/Vertex.cs
+++ WpfUI/Models/Vertex.cs
@@ -6,5 +6,13 @@
         public double X { get; set; } = x;
         public double Y { get; set; } = y;
         public double Weight { get; set; } = 0;
+
+        public override string ToString()
+        {
+            var result = ( Weight == 0 ) ? 
+                $"Имя {Name:16} | Положение ({X:f2}; {Y:f2})" : 
+                $"Имя {Name:16} | Положение ({X:f2}; {Y:f2}) | Вес {Weight}";
+            return result;
+        }
     }
 }
