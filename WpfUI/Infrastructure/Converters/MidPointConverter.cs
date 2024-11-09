9750264e45db55aaf83bb1f70f9d4a507753b07e
iliami <ilia.mi1908@gmail.com>
2024-11-09 19:27:35 +0400

    Добавил отображение и изменение имени и веса элементов графа в контекстном меню, доступному по правому клику по элементу.
    Вес ребра отображается только если он больше 0.
    
    Для отображения веса в центре ребра (линии) добавил конвертер.

---
+++ WpfUI/Infrastructure/Converters/MidPointConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfUI.Infrastructure.Converters
+{
+    public class MidPointConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 3 && values[0] is double start && values[1] is double end && values[2] is double actualSize)
+            {
+                return ( start + end - actualSize ) / 2.0;
+            }
+            return 0.0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return [];
+        }
+    }
+
+}
